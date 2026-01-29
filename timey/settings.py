3c5fb0a24631f3daadf12c60e186abfaf17bfb00
Alijon <boymirzayevalijon32@gmail.com>
2026-01-29 23:25:47 +0500

    initial

--- timey/settings.py
+++ timey/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w&1xo0u7xcrvh7&62p(=^q5@vf+-1%f7atwa(5$11x+$z%=)xz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
