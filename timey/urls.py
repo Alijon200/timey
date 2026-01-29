5824bc279a68e2520ce930e71166a67c403e52a4
Alijon <boymirzayevalijon32@gmail.com>
2026-01-29 23:23:28 +0500

    initial

--- timey/urls.py
+++ timey/urls.py
@@ -19,9 +19,14 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Timey!")
 
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/auth/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/auth/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
