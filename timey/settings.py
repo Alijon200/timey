b3a43619381f97fa066fe9a8c3fb934b23ecf587
Alijon <boymirzayevalijon32@gmail.com>
2026-01-30 01:13:48 +0500

    initial commit

--- timey/settings.py
+++ timey/settings.py
@@ -76,12 +76,13 @@ WSGI_APPLICATION = 'timey.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
 import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True,
     )
 }
 
